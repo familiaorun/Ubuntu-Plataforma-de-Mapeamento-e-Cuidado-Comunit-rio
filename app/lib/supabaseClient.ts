a84357adc0666b1f0bed9704237ef1a31df9c0df
Daniel Carvalho <danielcarvalho@Daniels-MacBook-Pro.local>
2026-01-27 20:11:25 -0300

    fix supabaseClient env parsing

--- app/lib/supabaseClient.ts
+++ app/lib/supabaseClient.ts
@@ -1,6 +1,9 @@
-\import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URLhttps://ifkxlpndhczerpxbaigv.supabase.co
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY sb_publishable_4k74T-hffZz6BSG46T4Cpg_1rMB2GLu
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+if (!supabaseUrl) throw new Error('Missing env: NEXT_PUBLIC_SUPABASE_URL')
+if (!supabaseAnonKey) throw new Error('Missing env: NEXT_PUBLIC_SUPABASE_ANON_KEY')
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
